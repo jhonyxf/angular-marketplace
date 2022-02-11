bab9f13c01491378164e3624965e99d60ed6d922
jhonyxf <jhonyxf@gmail.com>
2022-02-11 20:23:11 -0200

    first commit

---
+++ src/main/java/com/jhony/crudspringangular12/CrudSpringAngular12Application.java
@@ -0,0 +1,13 @@
+package com.jhony.crudspringangular12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringAngular12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringAngular12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhony/crudspringangular12/CrudSpringAngular12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhony.crudspringangular12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAngular12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
